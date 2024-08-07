d85b918330c48ce13ea92fcc5de9e5154afd95ca
hetot <shaxzodmurtazayev05@gmail.com>
2024-08-07 03:04:51 +0300

    done

--- task-1-matrix-product/src/main/java/com/github/javarar/matrix/production/MatrixProduction.java
+++ task-1-matrix-product/src/main/java/com/github/javarar/matrix/production/MatrixProduction.java
@@ -1,13 +1,88 @@
 package com.github.javarar.matrix.production;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
 public class MatrixProduction {
 
     public static Matrix product(Matrix a, Matrix b) {
-        throw new UnsupportedOperationException("реализуй меня");
+        int leftColsNum = a.getColsNum();
+        int rightRowsNum = b.getRowsNum();
+        if (leftColsNum != rightRowsNum) {
+            throw new IllegalArgumentException();
+        }
+        var task = new ComputeResultRunnable(a, b);
+        var forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(task);
     }
 
+    @RequiredArgsConstructor
+    private static class ComputeResultRunnable extends RecursiveTask<Matrix> {
+        private final Matrix a, b;
+
+        @Override
+        protected Matrix compute() {
+            int rows = a.getRowsNum();
+            int cols = b.getColsNum();
+            var resultMatrix = new Matrix(rows, cols);
+            for (int i = 1; i <= resultMatrix.getRowsNum(); i++) {
+                for (int j = 1; j <= resultMatrix.getColsNum(); j++) {
+                    var task = new ComputeRowColResult(a, b, i, j);
+                    task.fork();
+                    resultMatrix.put(i, j, task.join());
+                }
+            }
+            return resultMatrix;
+        }
+
+        @RequiredArgsConstructor
+        private static class ComputeRowColResult extends RecursiveTask<Integer> {
+            private final Matrix a, b;
+            private final int i, j;
+
+            @Override
+            protected Integer compute() {
+                int sum = 0;
+                for (int k = 1; k <= i; k++) {
+                    sum += (a.get(i, k) * b.get(k, j));
+                }
+                return sum;
+            }
+        }
+    }
 
     public static class Matrix {
-        // здесь можно реализовать все методы, которые необъодимы для работы с матрицами
+        private final Map<String, Integer> matrix = new ConcurrentHashMap<>();
+        @Getter
+        private final int rowsNum;
+        @Getter
+        private final int colsNum;
+
+        public Matrix(int row, int col) {
+            if (row <= 0 || col <= 0) {
+                throw new IllegalArgumentException();
+            }
+            this.rowsNum = row;
+            this.colsNum = col;
+        }
+
+        public Integer get(int row, int col) {
+            validate(row, col);
+            return matrix.getOrDefault((row - 1) + "_" + (col - 1), 0);
+        }
+
+        public void put(int row, int col, Integer value) {
+            validate(row, col);
+            matrix.put((row - 1) + "_" + (col - 1), value);
+        }
+
+        private void validate(int row, int col) {
+            if (row < 0 || row >= rowsNum || col < 0 || col >= colsNum) {
+                throw new IllegalArgumentException("row: " + row + ", col: " + col);
+            }
+        }
     }
 }
--- task-2-animal-faces/src/main/java/com/github/javarar/animal/faces/AnimalFacesPipeline.java
+++ task-2-animal-faces/src/main/java/com/github/javarar/animal/faces/AnimalFacesPipeline.java
@@ -1,9 +1,85 @@
 package com.github.javarar.animal.faces;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class AnimalFacesPipeline {
-    // загрузка файлов из дерриктории (можно параллельно)
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
 
-    // изменение картинки (можно параллельно, рабиение зависит от метода трансформации)
+        Path resources = Paths.get("task-2-animal-faces/src/main/resources/afhq/train/cat");
 
-    // запись измененных картинок на диск
+        double rotationAngle = Math.toRadians(90);
+
+        Set<Future<?>> futures = new HashSet<>();
+        for (var imgName : listFilesUsingJavaIO(resources.toAbsolutePath().toString())) {
+            var future = executorService.submit(() -> {
+                try (var is = Files.newInputStream(resources.toAbsolutePath().resolve(imgName))) {
+                    BufferedImage image = ImageIO.read(is);
+
+                    int width = image.getWidth();
+                    int height = image.getHeight();
+                    int newWidth = (int) Math.abs(width * Math.cos(rotationAngle)) + (int) Math.abs(height * Math.sin(rotationAngle));
+                    int newHeight = (int) Math.abs(height * Math.cos(rotationAngle)) + (int) Math.abs(width * Math.sin(rotationAngle));
+
+                    BufferedImage outputImage = new BufferedImage(newWidth, newHeight, image.getType());
+                    Graphics2D g2d = outputImage.createGraphics();
+                    AffineTransform transform = new AffineTransform();
+                    transform.rotate(rotationAngle, (double) newWidth / 2, (double) newHeight / 2);
+                    g2d.setTransform(transform);
+                    g2d.drawImage(image, 0, 0, null);
+                    g2d.dispose();
+                    File outputFile = new File(resources.toAbsolutePath().resolve(imgName).toString());
+                    ImageIO.write(outputImage, "jpg", outputFile);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            futures.add(future);
+        }
+
+        int done = 0;
+        int size = futures.size();
+        while (!futures.isEmpty()) {
+            Set<Future<?>> toRemove = new HashSet<>();
+            for (Future<?> future : futures) {
+                try {
+                    if (future.isDone()) {
+                        future.get();
+                        done++;
+                        toRemove.add(future);
+                    }
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                    toRemove.add(future);
+                }
+            }
+            futures.removeAll(toRemove);
+        }
+        System.out.println("Done:" + done + " / " + size);
+        executorService.shutdown();
+    }
+
+    private static Set<String> listFilesUsingJavaIO(String dir) {
+        return Stream.of(new File(dir).listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toSet());
+    }
 }
